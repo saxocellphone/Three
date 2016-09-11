329ff529fa20cbbed27761a1e827071e9983b787
Wliu <Wliu1402@gmail.com>
2016-09-11 15:34:02 -0400

    Add examples

--- js/examples.js
+++ js/examples.js
@@ -1,4 +1,65 @@
-examples()
+window.addEventListener("keyup", onKeyUp);
+
+function showExamples() // eslint-disable-line
 {
-	// TODO
+	if(document.getElementById("modal").style.display === "none" || document.getElementById("modal").style.display === "")
+	{
+		document.getElementById("modal").style.display = "flex";
+	}
+}
+
+function hideExamples()
+{
+	if(document.getElementById("modal").style.display === "flex")
+	{
+		document.getElementById("modal").style.display = "none";
+	}
+}
+
+function tryMe(index) // eslint-disable-line
+{
+	// TODO: Fill these in programatically.
+	switch(index)
+	{
+		case 0:
+			document.getElementById("function1").value = "y=x";
+			document.getElementById("function2").value = "y=x^2";
+			document.getElementById("bound1").value = "x=1";
+			document.getElementById("bound2").value = "x=5";
+			document.getElementById("rotation").value = "y=0";
+			hideExamples();
+			break;
+		case 1:
+			document.getElementById("function1").value = "y=3";
+			document.getElementById("function2").value = "y=-x^2";
+			document.getElementById("bound1").value = "x=-3";
+			document.getElementById("bound2").value = "x=3";
+			document.getElementById("rotation").value = "y=4";
+			hideExamples();
+			break;
+		case 2:
+			document.getElementById("function1").value = "x=y";
+			document.getElementById("function2").value = "x=sqrt(y)";
+			document.getElementById("bound1").value = "y=1";
+			document.getElementById("bound2").value = "y=5";
+			document.getElementById("rotation").value = "x=1";
+			hideExamples();
+			break;
+		case 3:
+			document.getElementById("function1").value = "y=sin(x)";
+			document.getElementById("function2").value = "";
+			document.getElementById("bound1").value = "x=0";
+			document.getElementById("bound2").value = "x=pi";
+			document.getElementById("rotation").value = "y=0";
+			hideExamples();
+			break;
+	}
+}
+
+function onKeyUp(event)
+{
+	if(event.key === "Escape")
+	{
+		hideExamples();
+	}
 }
